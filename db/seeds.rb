1c4d87db3edfa9b1a04956cda912fb4988410180
Samuel Mullen <samuel@samuelmullen.com>
2025-07-02 19:52:59 -0500

    Seed DB from starting dataset

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,45 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "csv"
+
+path = File.join([Rails.root, "db/nodes.csv"])
+csv = CSV.read(path, converters: :integer)
+csv.shift # Remove the header row
+
+# sort the records by the second column, and then by the first column
+records = csv.sort do |a, b|
+  if (a[1].presence || a[0]) == (b[1].presence || b[0])
+    a[0] <=> b[0]
+  else
+    (a[1].presence || a[0]) <=> (b[1].presence || b[0])
+  end
+end
+
+records.each do |row|
+  # Create a Node record for each row in the CSV file
+  Node.create(id: row[0])
+
+  if row[1].blank?
+    NodeTreePath.create(descendant_id: row[0], ancestor_id: row[0])
+  else
+    NodeTreePath.create(descendant_id: row[0], ancestor_id: row[0])
+
+    NodeTreePath.where(descendant_id: row[1]).pluck(:ancestor_id).each do |ancestor_id|
+      NodeTreePath.create(descendant_id: row[0], ancestor_id: ancestor_id)
+    end
+  end
+end
+
+# records = csv.sort { |a,b|
+#   (a[1].presence || a[0]) <=> (b[1].presence || b[0])
+# }
+
+# CSV.foreach(File.join([Rails.root, "db/nodes.csv"]), headers: true) do |row|
+#   # Node.create(id: row["id"])
+# end
+
+# CSV.foreach(File.join([Rails.root, "db/nodes.csv"]), headers: true) do |row|
+#   # Node.create(id: row["id"])
+# end
