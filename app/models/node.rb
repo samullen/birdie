6b4bdb89f2e63287e55a90cc87fd303e7de1b640
Samuel Mullen <samuel@samuelmullen.com>
2025-07-03 13:12:08 -0500

    another tweak to node relations

--- app/models/node.rb
+++ app/models/node.rb
@@ -2,8 +2,8 @@ class Node < ApplicationRecord
   has_many :descendant_paths, class_name: 'NodeTreePath', foreign_key: 'ancestor_id'
   has_many :ancestor_paths, class_name: 'NodeTreePath', foreign_key: 'descendant_id'
 
-  has_many :descendants, class_name: 'Node', through: :ancestor_paths, source: :ancestor
-  has_many :ancestors, class_name: 'Node', through: :descendant_paths, source: :descendant
+  has_many :descendants, class_name: 'Node', through: :ancestor_paths, source: :descendant
+  has_many :ancestors, class_name: 'Node', through: :descendant_paths, source: :ancestor
 
   def common_ancestors(other_node)
     self.ancestor_paths.where(ancestor_id: other_node.ancestor_paths.pluck(:ancestor_id))
